57b8f39208148cfbe244388eaf43174e5b2a1024
sheldonkwoodward <sheldon.k.woodward@gmail.com>
2019-07-27 20:32:00 -0800

    Added support for whitespace in filenames.

--- flac-to-alac.sh
+++ flac-to-alac.sh
@@ -7,15 +7,17 @@
 # get the input and output directories
 FLAC_DIR=$1
 ALAC_DIR=$2
+# recursively create the ALAC dirs if they don't exist
+mkdir -p "$ALAC_DIR"
 # change to the FLAC directory
-cd $FLAC_DIR
+cd "$FLAC_DIR"
 # convert every FLAC file to ALAC and embed the album artwork
 for f in *.flac
 do
     # audio conversion
-    ffmpeg -i "$f" -vn -c:a alac -y "$ALAC_DIR/${f%.flac}.m4a" $ALAC_DIR/folder.png
+    ffmpeg -i "$f" -vn -c:a alac -y "$ALAC_DIR/${f%.flac}.m4a" "$ALAC_DIR/folder.png"
     # embed the artwork
-    # atomicparsley "$ALAC_DIR/${f%.flac}.m4a" --artwork $ALAC_DIR/cover.png --overWrite
+    # atomicparsley "$ALAC_DIR/${f%.flac}.m4a" --artwork "$ALAC_DIR/cover.png" --overWrite
 done
 # remove the extracted album artwork
-# rm $ALAC_DIR/cover.png
+# rm "$ALAC_DIR/cover.png"
